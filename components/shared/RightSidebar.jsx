60b381bab1b9357d74b5a3329e857234aefd4171
arnold-aswan <fredyaswan@gmail.com>
2024-06-03 15:59:59 +0300

    added right sidebar

---
+++ components/shared/RightSidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <section className="sticky top-0 right-0 z-20 text-white h-full px-6">
+      <div>
+        <h3 className="font-bold">Suggested Communities</h3>
+      </div>
+      <div>
+        <h3 className="font-bold">Suggested People</h3>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
