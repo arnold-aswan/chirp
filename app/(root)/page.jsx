749da84acec07ddcf07ec0e3196846b2c61a12e7
arnold-aswan <fredyaswan@gmail.com>
2024-06-03 16:02:23 +0300

    removed sidebar component from homepage

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,10 +1,9 @@
-
 import SideBar from "@/components/shared/SideBar";
 
 export default function Home() {
   return (
-    <main className="max-w-screen-2xl container mx-auto">
-      <SideBar />
+    <main className="max-w-screen-2xl  container mx-auto flex">
+      <h1 className="head-text">Home </h1>
     </main>
   );
 }
