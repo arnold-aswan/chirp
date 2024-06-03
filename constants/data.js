32386062ac82157002aee2b171e2e43d244dba2a
arnold-aswan <fredyaswan@gmail.com>
2024-06-03 15:58:33 +0300

    added more links to constants

--- constants/data.js
+++ constants/data.js
@@ -3,11 +3,15 @@ import {
   UserIcon,
   PlusCircleIcon,
   UserGroupIcon,
+  MagnifyingGlassIcon,
+  HeartIcon,
 } from "@heroicons/react/24/outline";
 
 export const sidebarLiks = [
   { name: "home", icon: <HomeIcon />, href: "/" },
-  { name: "create", icon: <PlusCircleIcon />, href: "/post" },
+  { name: "search", icon: <MagnifyingGlassIcon />, href: "/search" },
+  { name: "create", icon: <PlusCircleIcon />, href: "/create-post" },
+  { name: "activity", icon: <HeartIcon />, href: "/activity" },
   { name: "profile", icon: <UserIcon />, href: "/profile" },
   { name: "community", icon: <UserGroupIcon />, href: "/community" },
 ];
