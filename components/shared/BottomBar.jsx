d59ea8a06cf6a716484a29fc83d0ee716997bf84
arnold-aswan <fredyaswan@gmail.com>
2024-06-03 15:59:41 +0300

    added active link color and logout button

--- components/shared/BottomBar.jsx
+++ components/shared/BottomBar.jsx
@@ -1,20 +1,29 @@
+"use client";
+
 import React from "react";
 import { sidebarLiks } from "@/constants/data";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const BottomBar = () => {
+  const pathname = usePathname();
   return (
-    <>
-      {sidebarLiks.map((link) => (
-        <Link
-          href={link.href}
-          key={link.name}
-          className="flex flex-col items-center gap-2 hover:bg-[#3d3d4e] hover:text-white px-3 py-2 rounded-lg w-fit ">
-          <div className="size-7">{link.icon}</div>
-          <p className="capitalize">{link.name}</p>
-        </Link>
-      ))}
-    </>
+    <section className="md:hidden bg-black text-white fixed bottom-0 right-0 left-0 flex items-center justify-around w-full py-2 ">
+      {sidebarLiks.map((link) => {
+        const isActive = pathname.includes(link.href) && pathname === link.href;
+        return (
+          <Link
+            href={link.href}
+            key={link.name}
+            className={`${
+              isActive && "bg-purple-500"
+            } flex flex-col items-center gap-2 hover:bg-purple text-white px-3 sm:px-5 py-2 rounded-lg`}>
+            <div className="size-7">{link.icon}</div>
+            <p className="capitalize hidden sm:flex ">{link.name}</p>
+          </Link>
+        );
+      })}
+    </section>
   );
 };
 
--- components/shared/SideBar.jsx
+++ components/shared/SideBar.jsx
@@ -1,23 +1,44 @@
+"use client";
 import React from "react";
-
 import { sidebarLiks } from "@/constants/data";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 const SideBar = () => {
+  const pathname = usePathname();
+
   return (
-    <>
-      <div className="hidden lg:flex lg:flex-col lg:gap-4">
-        {sidebarLiks.map((link) => (
-          <Link
-            href={link.href}
-            key={link.name}
-            className="flex items-center gap-2 hover:bg-[#3d3d4e] hover:text-white px-3 py-2 rounded-lg w-fit ">
-            <div className="size-7">{link.icon}</div>
-            <p className="capitalize">{link.name}</p>
-          </Link>
-        ))}
+    <section className="hidden sticky top-0 left-0 z-20 text-white md:flex md:flex-col pt-12 px-6 h-screen ">
+      <div className="flex flex-col gap-8">
+        {sidebarLiks.map((link) => {
+          const isActive =
+            pathname.includes(link.href) && pathname === link.href;
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={`${
+                isActive && "bg-purple"
+              } flex items-center gap-6 hover:bg-purple px-3 py-2 rounded-lg `}>
+              <div className="size-7 text-white">{link.icon}</div>
+              <p className="capitalize md:hidden lg:flex">{link.name}</p>
+            </Link>
+          );
+        })}
       </div>
-    </>
+      <div className="mt-10">
+        <SignedIn>
+          <SignOutButton redirectUrl="/sign-in">
+            <div className="flex items-center gap-4 cursor-pointer">
+              <Image src="/logout.svg" alt="logout" width={24} height={24} />
+              <p className="md:hidden lg:flex">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
   );
 };
 
