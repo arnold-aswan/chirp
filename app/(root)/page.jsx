054659257b43543ea375c11a33950cc33533ff8f
arnold-aswan <fredyaswan@gmail.com>
2024-06-06 18:44:29 +0300

    render posts on home page

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,9 +1,35 @@
-import SideBar from "@/components/shared/SideBar";
+import { fetchPosts } from "@/lib/actions/post.action";
+import { currentUser } from "@clerk/nextjs/server";
+import PostCard from "@/components/PostCard";
 
-export default function Home() {
+export default async function Home() {
+  const results = await fetchPosts(1, 20);
+  const user = await currentUser();
+  // console.log(results);
   return (
-    <main className="max-w-screen-2xl  container mx-auto flex">
+    <main className="">
       <h1 className="head-text">Home </h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {results.posts.length === 0 ? (
+          <p className="mt-9 text-white">No posts found</p>
+        ) : (
+          <>
+            {results.posts.map((post) => (
+              <PostCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </main>
   );
 }
