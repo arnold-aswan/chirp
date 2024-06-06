b9f245bafc587027dfeeaa608f12e0888a2e48bb
arnold-aswan <fredyaswan@gmail.com>
2024-06-06 18:46:30 +0300

    create post page

--- app/(root)/create-post/page.jsx
+++ app/(root)/create-post/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { currentUser } from "@clerk/nextjs/server";
+import AddPost from "@/components/forms/AddPost";
+import { isOnboarded } from "@/lib/utils";
 
-const page = () => {
+const page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await isOnboarded(user.id);
+  if (!userInfo) return null;
+
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <h1 className="head-text">Add a Post</h1>
 
-export default page
+      <AddPost userId={userInfo._id.toString()} />
+    </>
+  );
+};
+
+export default page;
