8488f1e4d4aa66e03b16bdec98ec8608e4f3e086
arnold-aswan <fredyaswan@gmail.com>
2024-06-06 18:42:39 +0300

    display hidden on mobile devices

--- components/shared/RightSidebar.jsx
+++ components/shared/RightSidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RightSidebar = () => {
   return (
-    <section className="sticky top-0 right-0 z-20 text-white h-full px-6">
+    <section className="hidden sticky top-0 right-0 z-20 text-white h-full px-6 md:flex md:flex-col">
       <div>
         <h3 className="font-bold">Suggested Communities</h3>
       </div>
