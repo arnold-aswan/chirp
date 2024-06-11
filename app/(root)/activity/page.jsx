b36c55824688594a60c5ed2e4e7814cef6e98d00
arnold-aswan <fredyaswan@gmail.com>
2024-06-11 17:27:00 +0300

    activities page

--- app/(root)/activity/page.jsx
+++ app/(root)/activity/page.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
+import Image from "next/image";
 
-const page = () => {
+const page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activities = await getActivity(userInfo._id);
+  console.log(activities);
   return (
-    <div>page</div>
-  )
-}
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+      {activities?.length > 0 ? (
+        <section className="mt-10 flex flex-col gap-5">
+          {activities.map((activity) => (
+            <Link key={activity._id} href={`/post/${activity.parentId}`}>
+              <article>
+                <Image
+                  src={activity.author.image}
+                  alt="profile photo"
+                  width={20}
+                  height={20}
+                  className="rounded-full object-cover"
+                />
+              </article>
+            </Link>
+          ))}
+        </section>
+      ) : (
+        <p className="text-white">No activity yet</p>
+      )}
+    </section>
+  );
+};
 
-export default page
+export default page;
