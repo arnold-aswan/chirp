fdd46c090d14220429dba95d0e6ff77257466832
arnold-aswan <fredyaswan@gmail.com>
2024-06-11 13:22:58 +0300

    added profile tabs

--- constants/data.js
+++ constants/data.js
@@ -7,6 +7,9 @@ import {
   HeartIcon,
 } from "@heroicons/react/24/outline";
 
+import { BsChatDots, BsReply } from "react-icons/bs";
+import { FaTags } from "react-icons/fa";
+
 export const sidebarLiks = [
   { name: "home", icon: <HomeIcon />, href: "/" },
   { name: "search", icon: <MagnifyingGlassIcon />, href: "/search" },
@@ -14,4 +17,10 @@ export const sidebarLiks = [
   { name: "activity", icon: <HeartIcon />, href: "/activity" },
   { name: "profile", icon: <UserIcon />, href: "/profile" },
   { name: "community", icon: <UserGroupIcon />, href: "/community" },
+];
+
+export const profileTabs = [
+  { value: "posts", label: "Posts", icon: <BsChatDots /> },
+  { value: "replies", label: "Replies", icon: <BsReply /> },
+  { value: "tagged", label: "Tagged", icon: <FaTags /> },
 ];
