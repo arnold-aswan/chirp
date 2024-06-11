8e054d7ccdbf6d3e2b652d3d8f75983fb2c310ff
arnold-aswan <fredyaswan@gmail.com>
2024-06-11 15:53:28 +0300

    optional chained props to remove undefined errors

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -13,22 +13,25 @@ const PostCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }) => {
+
+
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article className={`flex w-full flex-col rounded-xl bg-dark-2 p-7`}>
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4 items-center">
-          <Link href={`/profile/${author.id}`} className="relative size-9">
+          <Link href={`/profile/${author?.id}`} className="relative size-9">
             <Image
-              src={author.image}
+              src={author?.image}
               alt="profile photo"
               fill
               className="cursor-pointer rounded-full"
             />
           </Link>
 
-          <Link href={`/profile/${author.id}`} className=" w-fit">
-            <h4 className="text-white">{author.fullname}</h4>
+          <Link href={`/profile/${author?.id}`} className=" w-fit">
+            <h4 className="text-white">{author?.fullname}</h4>
           </Link>
         </div>
       </div>
@@ -42,8 +45,16 @@ const PostCard = ({
           <div className="flex gap-4 text-gray-500">
             <FaRegHeart className="cursor-pointer" />
             <BsChatDots className="cursor-pointer" />
-            <BsReply className="cursor-pointer" />
+            <Link href={`/post/${id}`}>
+              <BsReply className="cursor-pointer" />
+            </Link>
           </div>
+
+          {isComment && comments?.length > 0 && (
+            <Link href={`/post/${id}`}>
+              <p className="text-white">{comments.length} replies</p>
+            </Link>
+          )}
         </div>
       </article>
     </article>
