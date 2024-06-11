568a0aadae6687feb2fc8a1ffddb26343277c427
arnold-aswan <fredyaswan@gmail.com>
2024-06-11 15:52:24 +0300

    added tabs profile page

--- app/(root)/profile/[id]/page.jsx
+++ app/(root)/profile/[id]/page.jsx
@@ -1,11 +1,62 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import PostsTab from "@/components/shared/PostsTab";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants/data";
+
+import { redirect } from "next/navigation";
 import { fetchUser } from "@/lib/actions/user.actions";
 
-const page = async ({ userId }) => {
-  const userInfo = await fetchUser(userId);
+const page = async ({ params }) => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <div>
       <h1 className="head-text">Profile</h1>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.fullname}
+        imageUrl={userInfo.image}
+        bio={userInfo.bio}
+        username={userInfo.username}
+      />
+
+      <div className="mt-9 ">
+        <Tabs defaultValue="posts" className="w-full">
+          <TabsList className="bg-dark-3 flex justify-between">
+            {profileTabs.map((tab) => (
+              <TabsTrigger
+                key={tab.label}
+                value={tab.value}
+                className="flex gap-2 w-full">
+                {tab.icon}
+                <p className="hidden md:flex">{tab.label}</p>
+                {tab.label === "Posts" && (
+                  <p className="bg-purple text-white rounded-lg px-2 text-[10px]">
+                    {userInfo?.posts?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent key={tab.label} value={tab.value}>
+              <PostsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </div>
   );
 };
