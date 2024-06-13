79452a5ab0231a57ce41985d5e8090db550bf737
arnold-aswan <fredyaswan@gmail.com>
2024-06-13 14:04:54 +0300

    pass down media array as props

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -5,7 +5,7 @@ import PostCard from "@/components/cards/PostCard";
 export default async function Home() {
   const results = await fetchPosts(1, 20);
   const user = await currentUser();
-
+  
   return (
     <main className="">
       <h1 className="head-text">Home </h1>
@@ -13,7 +13,7 @@ export default async function Home() {
         {results.posts.length === 0 ? (
           <p className="mt-9 text-white">No posts found</p>
         ) : (
-          <>
+          <section className="flex flex-col items-center gap-5">
             {results.posts.map((post) => (
               <PostCard
                 key={post._id}
@@ -25,9 +25,10 @@ export default async function Home() {
                 community={post.community}
                 createdAt={post.createdAt}
                 comments={post.children}
+                media={post.media}
               />
             ))}
-          </>
+          </section>
         )}
       </section>
     </main>
